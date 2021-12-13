759b71b0271bcbfa9d0b5e61acae9da5db07c4ba
Bradley Grupczynski <8422702@isd535.org>
2021-12-13 08:58:08 -0600

    Added console messages to the functions for images and reset button

--- scripts/main.js
+++ scripts/main.js
@@ -5,37 +5,44 @@ function resetcart() {
     document.getElementById("image4").src= "images/milky_way.jpeg"
     document.getElementById("image5").src= "images/grazing_sheep.jpeg"
     document.getElementById("image6").src= "images/emerald_lake.jpeg"
+    console.log("Resetting cart!");
 }
 
 
 function changeimage1()
 {
     document.getElementById("image1").src= "images/cart.jpg"
+    console.log("Lake Mountains in Cart!");
 }
 
 function changeimage2()
 {
 document.getElementById("image2").src= "images/cart.jpg"
+console.log("Fresh Icebergs in cart!");
 }
 
 function changeimage3()
 {
 document.getElementById("image3").src= "images/cart.jpg"
+console.log("Colorful Hills in cart!");
 }
 
 function changeimage4()
 {
 document.getElementById("image4").src= "images/cart.jpg"
+console.log("Aurora Borealis in cart!");
 }
 
 function changeimage5()
 {
 document.getElementById("image5").src= "images/cart.jpg"
+console.log("Grazing Sheep in cart!");
 }
 
 function changeimage6()
 {
 document.getElementById("image6").src= "images/cart.jpg"
+console.log("Forest Mountains in cart!");
 }
 
 window.addEventListener('contextmenu', (event) => {
