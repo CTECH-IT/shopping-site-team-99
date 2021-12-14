010bbe4d1c899bd05674f9b493e824863faabb1b
Tennistoby <89853762+Tennistoby@users.noreply.github.com>
2021-12-14 09:43:21 -0600

    added thumbnail code in comment

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,46 @@
 const SERVER_URL = 'http://saturn.rochesterschools.org:8080/json'
+const THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+/*
+function setDetails(imageUrl, titleText) {
+    'use strict';
+    let detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+    detailImage.setAttribute('src', imageUrl);
+
+    let detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
+    detailTitle.textContent = titleText;
+}
+
+function imageFromThumb(thumbnail) {
+    'use strict'; 
+    return thumbnail.getAttribute('data-image-url');
+}
+
+function sendDetailsFromThumb(thumbnail) {
+    'use strict';
+    setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
+}
+
+function addThumbClickHandler(thumb) {
+    'use strict';
+    thumb.addEventListener('click', function(event) {
+        event.preventDefault(); // stop browser from following href
+        setDetailsFromThumb(thumb);
+    }); 
+}
+
+function getThumbnailsArray() {
+    'use strict';
+    let thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
+    let thumbnailArray = [].slice.call(thumbnails); // convert Nodelist to array
+    return thumbnailArray;
+}
+
+function initializeEvents() {
+    'use strict';
+    let thumbnails = getThumbnailsArray();
+    thumbnails.forEach(addThumbClickHandler);
+}
+*/
 
 function resetcart() {
     document.getElementById("image1").src= "images/banff.jpeg"
@@ -13,8 +55,6 @@ function resetcart() {
     console.log(price);
     price = 0;
 }
-
-
 
 function submitcart() {
     document.getElementById("image1").src= "images/banff.jpeg"
